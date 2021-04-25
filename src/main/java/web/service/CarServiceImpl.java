9aeb4028dc76d8d8a1aa51b63286667373a27274
AlexandrBeybulatov <53230560+AlexandrBeybulatov@users.noreply.github.com>
2021-04-25 11:00:42 +0200

    rev

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,40 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    @Override
+    public List<Car> getCarCount(Integer count) {
+
+        if (count == null) {
+            count = 0;
+        }
+        List<Car> carList = new ArrayList<>();
+
+        carList.add(new Car(1, "bmw", "white"));
+        carList.add(new Car(2, "ferrari", "red"));
+        carList.add(new Car(3, "alfa", "green"));
+        carList.add(new Car(4, "suzuki", "gray"));
+        carList.add(new Car(5, "mazda", "gold"));
+
+        if (count==1) {
+            carList = carList.subList(0, 1);
+        } else if(count==2){
+            carList = carList.subList(0, 2);
+        } else if(count==3){
+            carList = carList.subList(0, 3);
+        } else if(count==4){
+            carList = carList.subList(0, 4);
+        } else if(count==5){
+            carList = carList.subList(0, 5);
+        } else if (count>5) {
+           carList = carList; }
+        return carList;
+    }
+}
